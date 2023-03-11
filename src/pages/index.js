5801221408269a05d70631141aa5daf85c47679f
Anmol Agrawal <anmol2002.aa1@gmail.com>
2023-03-11 16:56:16 +0530

    added heroImage-50%

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,41 +31,49 @@ const IndexPage = ({ data }) => {
 
   return (
     <Layout>
-    <Container>
-      <Filters>
-        <StatusFilter
-          value={statusFilter}
-          onChange={handleStatusFilterChange}
-        >
-          <option value="all">All</option>
-          <option value="stable">Stable</option>
-          <option value="beta">Beta</option>
-          <option value="alpha">Alpha</option>
-        </StatusFilter>
-        <VideosFilter
-          checked={videosFilter}
-          onChange={handleVideosFilterChange}
-        />
-        <VideosFilterLabel htmlFor="videos-filter">
-          Videos Available
-        </VideosFilterLabel>
-      </Filters>
-      <Modules>
-        {modules.map(module => (
-          <Module key={module.id}>
-            <Name dangerouslySetInnerHTML={{ __html: module.name }} />
-            <Description
-              dangerouslySetInnerHTML={{ __html: module.description }}
-            />
-            <Links>
-              <Link href={module.repository}>Repository</Link>
-              <Link href={module.webpage}>Webpage</Link>
-              {module.videos && <Link href={module.videos}>Videos</Link>}
-            </Links> 
-          </Module>
-        ))}
-      </Modules>
-    </Container></Layout>
+      <Container>
+        <Filters>
+          <StatusFilter
+            value={statusFilter}
+            onChange={handleStatusFilterChange}
+          >
+            <option value="all">All</option>
+            <option value="stable">Stable</option>
+            <option value="beta">Beta</option>
+            <option value="alpha">Alpha</option>
+          </StatusFilter>
+          <VideosFilter
+            checked={videosFilter}
+            onChange={handleVideosFilterChange}
+          />
+          <VideosFilterLabel htmlFor="videos-filter">
+            Videos Available
+          </VideosFilterLabel>
+        </Filters>
+        <Modules>
+          {modules.map((module) => (
+            <Module key={module.id}>
+              {module.heroImage && (
+                <ModuleImage>
+                  <HeroImage src={module.heroImage} alt={module.name} />
+                </ModuleImage>
+              )}
+              <ModuleContent>
+                <Name dangerouslySetInnerHTML={{ __html: module.name }} />
+                <Description
+                  dangerouslySetInnerHTML={{ __html: module.description }}
+                />
+                <Links>
+                  <Link href={module.repository}>Repository</Link>
+                  <Link href={module.webpage}>Webpage</Link>
+                  {module.videos && <Link href={module.videos}>Videos</Link>}
+                </Links>
+              </ModuleContent>
+            </Module>
+          ))}
+        </Modules>
+      </Container>
+    </Layout>
   );
 };
 
@@ -79,6 +87,7 @@ export const query = graphql`
         status
         videos
         webpage
+        heroImage
         id
       }
     }
@@ -99,16 +108,17 @@ const Filters = styled.div`
 
 const StatusFilter = styled.select`
   margin-right: 16px;
-  aria-label: "Filter by status";
 `;
 
-const VideosFilter = styled.input.attrs({ type: "checkbox", id: "videos-filter" })`
+const VideosFilter = styled.input.attrs({
+  type: "checkbox",
+  id: "videos-filter",
+})`
   margin-right: 8px;
 `;
 
 const VideosFilterLabel = styled.label`
   font-size: 16px;
-  aria-label: "Filter by videos available";
 `;
 
 const Modules = styled.div`
@@ -119,66 +129,80 @@ const Modules = styled.div`
   margin: 16px;
 `;
 
-const Module = styled.div`
+ const HeroImage = styled.img`
+    border-radius: 8px;
+    margin-bottom: 16px;
+    max-width: 100%;
+    width: 100%;
+    height: 200px;
+  `;
+  
+  const Module = styled.div`
+  display: flex;
   background-color: #fff;
   border-radius: 16px;
   box-shadow: 0 0 16px rgba(0, 0, 0, 0.1);
   padding: 24px;
-  width: 300px;
+  width: 100%;
   transition: all 0.2s ease-in-out;
 
   &:hover {
     transform: scale(1.05);
+    box-shadow: 0 0 24px rgba(0, 0, 0, 0.2);
+  }
+
+  @media (min-width: 768px) {
+    width: 300px;
   }
 `;
 
-const Name = styled.div`
-  font-size: 28px;
-  font-weight: bold;
-  margin-bottom: 8px;
-  color: #333;
+const ModuleImage = styled.div`
+  width: 40%;
+  margin-right: 16px;
+
+  @media (max-width: 767px) {
+    width: 100%;
+    margin-right: 0;
+    margin-bottom: 16px;
+  }
 `;
 
-const Description = styled.div`
-  font-size: 18px;
+const ModuleContent = styled.div`
+  width: 60%;
+
+  @media (max-width: 767px) {
+    width: 100%;
+  }
+`;
+
+const Name = styled.h2`
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 8px;
+`;
+
+const Description = styled.p`
+  font-size: 16px;
   margin-bottom: 16px;
-  color: #555;
 `;
 
 const Links = styled.div`
   display: flex;
   justify-content: space-between;
-  gap: 8px;
 `;
 
 const Link = styled.a`
-  color: #2f80ed;
-  font-size: 14px;
+  font-size: 16px;
+  color: #1e90ff;
   text-decoration: none;
   transition: all 0.2s ease-in-out;
 
   &:hover {
-    color: #1f65d6;
+    color: #0066cc;
+    text-decoration: underline;
   }
 `;
-
-const Status = styled.div`
-color: ${props => {
-  switch (props.status) {
-    case 'stable':
-      return '#4CAF50';
-    case 'beta':
-      return '#FFC107';
-    case 'alpha':
-      return '#F44336';
-    default:
-      return '#9E9E9E';
-  }
-}};
-font-size: 14px;
-font-weight: bold;
-margin-top: 8px;
-`;
-
-
-  export default IndexPage;
+    
+    export default IndexPage;
+    
+    
