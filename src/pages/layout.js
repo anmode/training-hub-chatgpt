65f2e88942a4a719c8fe0870c5f7f2f7c4f63899
Anmol Agrawal <anmol2002.aa1@gmail.com>
2023-03-12 09:50:56 +0530

    added a popup for mobile devices

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 import styled from "styled-components";
 
 const Layout = ({ children }) => {
+  const [showPopup, setShowPopup] = useState(true);
+
+
+  const handlePopupClose = () => {
+    setShowPopup(false);
+  };
+  useEffect(() => {
+    if (window.innerWidth < 768) { // Check if the screen width is less than 768px
+      setShowPopup(true); // Show the popup if the screen is small
+    }
+  }, []);
   return (
     <Container>
+      {showPopup && <Popup onClose={handlePopupClose} />}
       <Nav>
         <Logo>Training HUB</Logo>
         <NavLinks>
@@ -12,7 +24,10 @@ const Layout = ({ children }) => {
           <NavLink href="/contact">Contact</NavLink>
         </NavLinks>
       </Nav>
-      <Main>{children}</Main>
+      <Main>
+        {children}
+        <Button onClick={() => setShowPopup(true)}>Show Popup</Button>
+      </Main>
       <Footer>© 2023 Training Center</Footer>
     </Container>
   );
@@ -63,6 +78,21 @@ const Main = styled.div`
   padding: 32px;
 `;
 
+const Button = styled.button`
+  margin-top: 32px;
+  padding: 12px 16px;
+  background-color: #2f80ed;
+  color: #ffffff;
+  font-size: 16px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #2569c8;
+  }
+`;
+
 const Footer = styled.footer`
   background-color: #ffffff;
   padding: 16px;
@@ -70,5 +100,78 @@ const Footer = styled.footer`
   color: #666666;
   font-size: 14px;
 `;
+
+const Popup = ({ onClose }) => {
+  return (
+    <PopupContainer>
+      <PopupContent>
+        <PopupHeader>
+          <PopupTitle>Important Message</PopupTitle>
+          <PopupCloseButton onClick={onClose}>X</PopupCloseButton>
+        </PopupHeader>
+        <PopupMessage>
+          This site is not optimized for mobile viewing. Please visit on a desktop device for the best experience.
+        </PopupMessage>
+      </PopupContent>
+    </PopupContainer>
+  );
+};
+
+const PopupContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  @media screen and (min-width: 768px) {
+    display: none;
+  }
+`;
+
+
+const PopupContent = styled.div`
+  background-color: white;
+  border-radius: 4px;
+  padding: 24px;
+  width: 400px;
+  max-width: 90%;
+  text-align: center;
+`;
+
+const PopupHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 16px;
+`;
+
+const PopupTitle = styled.h3`
+  font-size: 24px;
+  margin: 0;
+`;
+
+const PopupCloseButton = styled.button`
+  background: none;
+  border: none;
+  font-size: 24px;
+  color: #666666;
+  cursor: pointer;
+
+  &:hover {
+    color: #2f80ed;
+  }
+`;
+
+const PopupMessage = styled.p`
+  font-size: 16px;
+  margin: 0;
+  line-height: 1.5;
+`;
+
 
 export default Layout;
